a6ba485d6768e4982c26db6128c11baa92a6f4f1
Aysun120 <aysundamar114@gmail.com>
2022-07-12 16:29:47 -0500

    LoginPage added

--- src/test/java/com/cydeo/library/pages/LoginPage.java
+++ src/test/java/com/cydeo/library/pages/LoginPage.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import java.util.List;
 
+// page_url = https://library2.cydeo.com/login.html
 public class LoginPage {
+    @FindBy(id = "inputEmail")
+    public WebElement emailInput;
 
-    @FindBy(xpath = "//label[text()='Email address']")
-    public WebElement usernameInput;
-
-    @FindBy(xpath = "//label[text()='Password']")
+    @FindBy(id = "inputPassword")
     public WebElement passwordInput;
 
     @FindBy(xpath = "//button")
     public WebElement signinBtn;
+
+    // No page elements added
 
     public LoginPage() {
         PageFactory.initElements(Driver.getDriver(), this);
--- src/test/java/com/cydeo/library/step_definitions/LoginStepDef.java
+++ src/test/java/com/cydeo/library/step_definitions/LoginStepDef.java
@@ -1,24 +1,26 @@
 package com.cydeo.library.step_definitions;
 
+import com.cydeo.library.pages.LoginPage;
+import com.cydeo.library.utilities.ConfigurationReader;
+import com.cydeo.library.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDef {
 
+    LoginPage loginPage = new LoginPage();
+
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get(ConfigurationReader.getProperty("qa2_url"));
     }
     @When("I login as a librarian")
     public void i_login_as_a_librarian() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("dashboard should be displayed")
     public void dashboard_should_be_displayed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
